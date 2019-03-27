8a74d806d4b928af174334c0e6b49d088ea9c8e0
cai879174373 <879174373@qq.com>
2019-03-27 14:50:59 +0800

    整合购物车

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,14 @@
               <a>退出</a>
               <strong>|</strong>
             </span>
-            <a href class>
+            <!-- <a href class> -->
+            <router-link to="/cart">
               <i class="iconfont icon-cart"></i>购物车(
               <span id="shoppingCartCount">
                 <span>4</span>
               </span>)
-            </a>
+              <!-- </a> -->
+            </router-link>
           </div>
         </div>
       </div>
@@ -39,9 +41,11 @@
           <div id="menu2" class="nav-box menuhd">
             <ul>
               <li class="index">
-                <a href="#" class>
+                <!-- <a href="#" class> -->
+                <router-link to="/index">
                   <span class="out" style="top: 0px;">首页</span>
-                </a>
+                  <!-- </a> -->
+                </router-link>
               </li>
               <li class="news">
                 <a href="#" class>
@@ -90,7 +94,7 @@
     </div>
 
     <!-- 页面区域 -->
- <router-view></router-view>
+    <router-view></router-view>
     <!-- 尾部区域 -->
 
     <div class="footer">
--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入路由
 import VueRouter from 'vue-router'
+// 使用路由
 Vue.use(VueRouter)
 
 import './assets/statics/site/css/style.css'
 
 
 import index from './components/index.vue'
-
+import cart from './components/cart.vue'
+// 规则
 let routes=[
   {path:'/index',component:index},
   {path:'/',component:index},
+  {path:'/cart',component:cart},
 ]
 let router =new VueRouter({
   routes

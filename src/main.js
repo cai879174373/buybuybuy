92e1a9c9a4e1f5b9d3d5e159b6dc8514fc88b76b
cai879174373 <879174373@qq.com>
2019-03-27 11:23:36 +0800

    整合头部和

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-
+import style from './assets/statics/site/css/style.css'
 Vue.config.productionTip = false
 
 new Vue({

2d9e6c12fc34f4b2a0c358f8022de2ff3d9a6748
cai879174373 <879174373@qq.com>
2019-03-30 20:41:43 +0800

    会员中心

---
+++ src/components/usercontainer.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="el-row">
+    <div class="el-col el-col-24">
+      <div class="comp">
+        <div class="tmpl routeanimate">
+          <div class="section">
+            <div class="location">
+              <span>当前位置：</span>
+              <a href="/index.html">首页</a> &gt;
+              <a href="/user/center/index.html">会员中心</a>
+            </div>
+          </div>
+          <div class="section clearfix">
+            <div class="left-260">
+              <div class="bg-wrap">
+                <div class="avatar-box">
+                  <a href="/user/center/avatar.html" class="img-box">
+                    <i class="iconfont icon-user-full"></i>
+                  </a>
+                  <h3>ivanyb</h3>
+                  <p>
+                    <b>注册会员</b>
+                  </p>
+                </div>
+                <div class="center-nav">
+                  <ul>
+                    <li>
+                      <h2>
+                        <i class="iconfont icon-order"></i>
+                        <span>订单管理</span>
+                      </h2>
+                      <div class="list">
+                        <p>
+                          <a href="#/site/member/orderlist" class>
+                            <i class="iconfont icon-arrow-right"></i>交易订单
+                          </a>
+                          <router-link to="/user/index">  <i class="iconfont icon-arrow-right"></i>首页</router-link>
+                          <router-link to="/user/detail">  <i class="iconfont icon-arrow-right"></i>订单详情</router-link>
+                        </p>
+                      </div>
+                    </li>
+                    <li>
+                      <h2>
+                        <i class="iconfont icon-user"></i>
+                        <span>账户管理</span>
+                      </h2>
+                      <div class="list">
+                        <p>
+                          <a href="#/site/member/center" class="router-link-exact-active">
+                            <i class="iconfont icon-arrow-right"></i>账户资料
+                          </a>
+                        </p>
+                        <p>
+                          <a href="#/site/member/center" class="router-link-exact-active">
+                            <i clrouter-linkss="iconfont icon-router-linkrrow-right"></i>头像设置
+                          </a>
+                        </p>
+                        <p>
+                          <a href="#/site/member/center" class="router-link-exact-active">
+                            <i class="iconfont icon-arrow-right"></i>修改密码
+                          </a>
+                        </p>
+                        <p>
+                          <a href="javascript:void(0)">
+                            <i class="iconfont icon-arrow-right"></i>退出登录
+                          </a>
+                        </p>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="right-auto">
+              <router-view></router-view>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -34,12 +34,27 @@ import './assets/statics/site/css/style.css'
 import index from './components/index.vue'
 import cart from './components/cart.vue'
 import detail from './components/detail.vue'
+import user from './components/usercontainer.vue'
+import userIndex from './components/userIndex.vue'
+import userDetail from './components/userDetail.vue'
 // 规则
 let routes=[
   {path:'/index',component:index},
   {path:'/',component:index},
   {path:'/cart',component:cart},
   {path:'/detail/:id',component:detail},
+  {path:'/user',component:user,
+  children:[
+    {
+    path:'index',
+    component:userIndex
+  },
+    {
+    path:'Detail',
+    component:userDetail
+  },
+]
+},
 ]
 let router =new VueRouter({
   routes
